e9e156bd792f256e8861c4c6d69eab255e3c7971
zee-creato <85754092+zee-creato@users.noreply.github.com>
2022-11-16 19:49:43 +0000

    first commit

---
+++ DomainModels/AddStudentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DomainModels
+{
+    public class AddStudentRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string DateofBirth { get; set; }
+
+        public string Email { get; set; }
+
+
+        public long Mobile { get; set; }
+
+
+
+        public Guid GenderId { get; set; }
+
+        public string PhysicalAddress { get; set; }
+
+
+        public string PostalAddress { get; set; }
+
+    }
+}
---
+++ Profiles/AfterMaps/UpdateStudentRequestAfterMap.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using DataModels = StudentAdminPortal.API.DataModels;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.DomainModels;
+
+namespace StudentAdminPortal.API.Profiles.AfterMaps
+{
+    public class UpdateStudentRequestAfterMap : IMappingAction<UpdateStudentRequest , DataModels.Student>
+    {
+       
+
+        public void Process(UpdateStudentRequest source, DataModels.Student destination, ResolutionContext context)
+        {
+            destination.Address = new DataModels.Address()
+            {
+                PhysicalAddress = source.PhysicalAddress,
+                PostalAddress = source.PostalAddress
+            };
+            
+        }
+    }
+}
